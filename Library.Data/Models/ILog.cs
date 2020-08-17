ccebb1e651c9ee68844a38f24622a8f40e6839a1
Akhil m j <akhilmjoy8@gmail.com>
2020-08-17 14:35:58 +0530

    adasdsd

---
+++ Library.Data/Models/ILog.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Library.Data.Models
+{
+    public interface ILog 
+    {
+        short? IsActive { get; set; } 
+
+        int? CreatedBy { get; set; }
+
+        int? UpdatedBy { get; set; }
+
+        DateTime? CreatedDate { get; set; }
+
+        DateTime? LastUpdatedDate { get; set; }
+    }
+}
